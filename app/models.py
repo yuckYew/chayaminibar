f8a0897f23ac71fb79b6b1e81bf634f27f16fbe7
yuckYew <yusukechayama@gmail.com>
2018-06-02 18:27:01 +0900

    add init stuff. add db model

---
+++ app/models.py
@@ -0,0 +1,38 @@
+from app import db
+
+class User(db.Model):
+    id      = db.Column(db.Integer, primary_key=True)
+    username    = db.Column(db.String(64), index=True, unique=True)
+    email   = db.Column(db.String(120), index=True, unique=True)
+    firebase_uid = db.Column(db.String(64), index=True, unique=True)
+    rank_id = db.Column(db.Integer, index=True)
+    
+class Rank(db.Model):
+    id      = db.Column(db.Integer, primary_key=True)
+    name    = db.Column(db.String(64), index=True, unique=True)
+
+class Admin(db.Model):
+    id      = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class ChashTrade(db.Model):
+    id      = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    price = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
+
+class Product(db.Model):
+    id      = db.Column(db.Integer, primary_key=True)
+    name    = db.Column(db.String(64), index=True, unique=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    price = db.Column(db.Integer, index=True)
+    barcode = db.Column(db.Integer, index=True)
+
+class ProductTrade(db.Model):
+    id      = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    name    = db.Column(db.String(64), index=True, unique=True)
+    user = db.Column(db.Integer, index=True, db.ForeignKey('user.id'))
+    price = db.Column(db.Integer, index=True, db.ForeignKey('product.price'))
+    chash_trade_id = db.Column(db.Integer, db.ForeignKey('chash_trade.id'))
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='chayaminibar'
+    version='0.0.0.0.1'
+    description='POS module for I lab'
+    author_email='example@example.com'
+    packages=['chayaminibar']
+    install_requires=['flask', 'flask-sqlalchemy', 'flask-migrate']
+)

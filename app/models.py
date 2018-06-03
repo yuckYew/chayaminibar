d8fb01fbc4d9803210b58d9ac551feb5a524b529
yuckyew <yusukechayama@gmail.com>
2018-06-04 02:20:50 +0900

    checked pep8

--- app/models.py
+++ app/models.py
@@ -1,42 +1,48 @@
 from app import db
 from datetime import datetime
 
+
 class User(db.Model):
-    id              = db.Column(db.Integer, primary_key=True)
-    username        = db.Column(db.String(64), index=True, unique=True)
-    email           = db.Column(db.String(120), index=True, unique=True)
-    firebase_uid    = db.Column(db.String(64), index=True, unique=True)
-    rank_id         = db.relationship('Rank', uselist=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    firebase_uid = db.Column(db.String(64), index=True, unique=True)
+    rank_id = db.relationship('Rank', uselist=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
+
+
 class Rank(db.Model):
-    id      = db.Column(db.Integer, primary_key=True)
-    name    = db.Column(db.String(64), index=True, unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
 
 class Admin(db.Model):
-    id      = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
+
 class ChashTrade(db.Model):
-    id          = db.Column(db.Integer, primary_key=True)
-    timestamp   = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id     = db.Column(db.Integer, db.ForeignKey('user.id'))
-    price       = db.Column(db.Integer)
-    quantity    = db.Column(db.Integer)
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    price = db.Column(db.Integer)
+    quantity = db.Column(db.Integer)
+
 
 class Product(db.Model):
-    id          = db.Column(db.Integer, primary_key=True)
-    name        = db.Column(db.String(64), index=True, unique=True)
-    price       = db.Column(db.Integer, index=True)
-    barcode     = db.Column(db.Integer, index=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index=True, unique=True)
+    price = db.Column(db.Integer, index=True)
+    barcode = db.Column(db.Integer, index=True)
+
 
 class ProductTrade(db.Model):
-    id              = db.Column(db.Integer, primary_key=True)
-    timestamp       = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    name            = db.Column(db.String(64), index=True, unique=True)
-    user            = db.Column(db.Integer, db.ForeignKey('user.id'))
-    price           = db.Column(db.Integer, db.ForeignKey('product.price'))
-    chash_trade_id  = db.Column(db.Integer, db.ForeignKey('chash_trade.id'))
+    id = db.Column(db.Integer, primary_key=True)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    name = db.Column(db.String(64), index=True, unique=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    price = db.Column(db.Integer, db.ForeignKey('product.price'))
+    chash_trade_id = db.Column(db.Integer, db.ForeignKey('chash_trade.id'))
--- app/routes.py
+++ app/routes.py
@@ -2,12 +2,15 @@ from app import app
 from flask_restful import Api, Resource
 from app.models import User, Product
 
+
 api = Api(app)
+
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, Yawara"
+
 
 class UserAPI(Resource):
     def get(self, id):
@@ -24,6 +27,7 @@ class UserAPI(Resource):
     def delete(self, id):
         pass
 
+
 class ProductAPI(Resource):
     def get(self, id):
         product = Product.query.filter_by(id=id).first()
@@ -39,12 +43,14 @@ class ProductAPI(Resource):
     def delete(self, id):
         pass
 
+
 class LoginAPI(Resource):
     def get(self, id):
         pass
 
     def post(self, id):
         pass
+
 
 api.add_resource(UserAPI,    '/api/users/<int:id>', endpoint='user')
 api.add_resource(ProductAPI, '/api/products/<int:id>', endpoint='product')
